a181c5cf8a5686d3b594ece034fb9ccab047153d
danhdelrey <duonghoaidanh21092003@gmail.com>
2025-06-06 08:58:59 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,30 @@
 import 'package:dishlocal/app/config/router.dart';
 import 'package:dishlocal/app/theme/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
+// Đặt hàm của bạn ở đây (hoặc import từ file khác)
+void overlaySystemNavigationBar() {
+  // Đảm bảo bạn đã import 'package:flutter/services.dart';
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      systemNavigationBarColor: Colors.transparent,
+      systemNavigationBarDividerColor: Colors.transparent,
+      systemNavigationBarContrastEnforced: false,
+      systemNavigationBarIconBrightness:
+          Brightness.dark, // Các icon (home, back) sẽ là màu tối
+      statusBarColor: Colors.transparent,
+      statusBarIconBrightness:
+          Brightness.dark, // Các icon trên status bar (pin, wifi) sẽ là màu tối
+    ),
+  );
+
+}
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  overlaySystemNavigationBar();
+  
   runApp(const MyApp());
 }
 
