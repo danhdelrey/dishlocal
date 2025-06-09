9f4afa29227116b8d1fcd8d4e7f287402b92ae7a
danhdelrey <duonghoaidanh21092003@gmail.com>
2025-06-09 10:23:26 +0700

    set up logging

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
+import 'dart:developer';
+
 import 'package:dishlocal/app/config/router.dart';
 import 'package:dishlocal/app/config/set_up_dependencies.dart';
 import 'package:dishlocal/app/theme/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:logging/logging.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await setUpDependencies();
+  _setupLogging();
   runApp(const MyApp());
 }
 
@@ -22,4 +26,23 @@ class MyApp extends StatelessWidget {
       routerConfig: router,
     );
   }
+}
+
+void _setupLogging() {
+  // Không cần kiểm tra kDebugMode nữa vì log() đã tự xử lý
+  Logger.root.level = Level.ALL;
+  Logger.root.onRecord.listen((record) {
+    // 2. Thay thế print() bằng log()
+    log(
+      // Message chính
+      '${record.loggerName}: ${record.message}',
+
+      // Các tham số tùy chọn của hàm log()
+      time: record.time,
+      level: record.level.value, // Level được biểu thị bằng số nguyên
+      name: record.loggerName,
+      error: record.error,
+      stackTrace: record.stackTrace,
+    );
+  });
 }
