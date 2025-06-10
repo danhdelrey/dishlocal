9672d8924217796132975603d07d6c9126db063f
danhdelrey <duonghoaidanh21092003@gmail.com>
2025-06-10 19:36:01 +0700

    2

--- lib/ui/features/camera/view/camera_page.dart
+++ lib/ui/features/camera/view/camera_page.dart
@@ -3,7 +3,7 @@ import 'package:dishlocal/app/theme/app_icons.dart';
 import 'package:dishlocal/app/theme/theme.dart';
 import 'package:dishlocal/ui/features/camera/bloc/camera_bloc.dart';
 import 'package:dishlocal/ui/features/get_current_location/view/current_location_builder.dart';
-import 'package:dishlocal/ui/features/internet_connection/view/internet_connection_handler.dart';
+import 'package:dishlocal/ui/features/internet_connection/view/internet_connection_builder.dart';
 import 'package:dishlocal/ui/features/internet_connection/view/internet_connection_disconnected_info.dart';
 import 'package:dishlocal/ui/features/location_service/view/location_service_disabled_info.dart';
 import 'package:dishlocal/ui/features/location_service/view/location_service_status_builder.dart';
@@ -48,14 +48,11 @@ class CameraPage extends StatelessWidget {
               ),
             ),
           ),
-          body: InternetConnectionHandler(
-            initialChild: const Center(
-              child: CustomLoadingIndicator(
-                indicatorSize: 40,
-                indicatorText: 'Đang kiểm tra kết nối...',
-              ),
-            ),
-            childWhenConnected: LocationServiceStatusBuilder(
+          body: InternetConnectionBuilder(builder: (hasInternetAccess) {
+            if (!hasInternetAccess) {
+              return const InternetConnectionDisconnectedInfo();
+            }
+            return LocationServiceStatusBuilder(
               builder: (enabled) {
                 if (enabled == false) {
                   return const LocationServiceDisabledInfo();
@@ -166,9 +163,8 @@ class CameraPage extends StatelessWidget {
                   },
                 );
               },
-            ),
-            childWhenDisconnected: const InternetConnectionDisconnectedInfo(),
-          ),
+            );
+          }),
         ),
       ),
     );
---
+++ lib/ui/features/internet_connection/view/internet_connection_builder.dart
@@ -0,0 +1,42 @@
+import 'package:dishlocal/ui/widgets/custom_loading_indicator.dart';
+import 'package:flutter/material.dart';
+import 'package:internet_connection_checker_plus/internet_connection_checker_plus.dart';
+
+class InternetConnectionBuilder extends StatelessWidget {
+  const InternetConnectionBuilder({
+    super.key,
+    required this.builder,
+  });
+
+  final Widget Function(bool hasInternetAccess) builder;
+
+  @override
+  Widget build(BuildContext context) {
+    // Dùng FutureBuilder để lấy trạng thái ban đầu
+    return FutureBuilder<bool>(
+      future: InternetConnection().hasInternetAccess,
+      builder: (context, futureSnapshot) {
+        // Nếu chưa lấy xong trạng thái ban đầu -> loading
+        if (futureSnapshot.connectionState == ConnectionState.waiting) {
+          return const Center(
+            child: CustomLoadingIndicator(indicatorSize: 40),
+          );
+        }
+
+        // Lấy trạng thái ban đầu từ future
+        final initialStatus = futureSnapshot.data ?? false;
+
+        // Dùng StreamBuilder để lắng nghe các thay đổi
+        return StreamBuilder<InternetStatus>(
+          stream: InternetConnection().onStatusChange,
+          // Cung cấp trạng thái ban đầu cho stream
+          initialData: initialStatus ? InternetStatus.connected : InternetStatus.disconnected,
+          builder: (context, streamSnapshot) {
+            final status = streamSnapshot.data!;
+            return builder(status == InternetStatus.connected);
+          },
+        );
+      },
+    );
+  }
+}
