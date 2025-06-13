3e02868aaa190226ef1efcd6da98be230ec67d76
danhdelrey <duonghoaidanh21092003@gmail.com>
2025-06-13 10:48:48 +0700

    1

--- lib/app/config/router.dart
+++ lib/app/config/router.dart
@@ -3,7 +3,7 @@ import 'package:dishlocal/data/categories/address/model/address.dart';
 import 'package:dishlocal/ui/features/camera/view/camera_page.dart';
 import 'package:dishlocal/ui/features/home/view/home_page.dart';
 import 'package:dishlocal/ui/features/login/view/login_page.dart';
-import 'package:dishlocal/ui/features/new_post/view/new_post_page.dart';
+import 'package:dishlocal/ui/features/dining_info_input/view/new_post_page.dart';
 import 'package:dishlocal/ui/features/profile/view/profile_page.dart';
 import 'package:dishlocal/ui/features/update_profile/view/account_setup_page.dart';
 import 'package:dishlocal/ui/features/view_post/view/post_detail_page.dart';
