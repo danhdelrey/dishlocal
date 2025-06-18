c0486190989cdebb97558aad9ccf5c3c13dad063
danhdelrey <duonghoaidanh21092003@gmail.com>
2025-06-18 16:23:05 +0700

    Update login_page.dart

--- lib/ui/features/auth/view/login_page.dart
+++ lib/ui/features/auth/view/login_page.dart
@@ -2,6 +2,7 @@ import 'package:dishlocal/app/theme/custom_colors.dart';
 import 'package:dishlocal/app/theme/app_icons.dart';
 import 'package:dishlocal/app/theme/theme.dart';
 import 'package:dishlocal/ui/features/auth/bloc/auth_bloc.dart';
+import 'package:dishlocal/ui/widgets/element_widgets/custom_loading_indicator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
@@ -12,94 +13,103 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SizedBox(
-        width: double.infinity,
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            const SizedBox(
-              height: 200,
-            ),
-            Column(
+      body: BlocBuilder<AuthBloc, AuthState>(
+        builder: (context, state) {
+          if (state is AuthLoading) {
+            return const Center(
+              child: CustomLoadingIndicator(indicatorSize: 40),
+            );
+          }
+          return SizedBox(
+            width: double.infinity,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
               children: [
-                Container(
-                  width: 100,
-                  height: 100,
-                  padding: const EdgeInsets.all(15),
-                  decoration: const BoxDecoration(
-                    gradient: primaryGradient,
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(28),
+                const SizedBox(
+                  height: 200,
+                ),
+                Column(
+                  children: [
+                    Container(
+                      width: 100,
+                      height: 100,
+                      padding: const EdgeInsets.all(15),
+                      decoration: const BoxDecoration(
+                        gradient: primaryGradient,
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(28),
+                        ),
+                      ),
+                      // Màu nền để dễ thấy
+                      child: AppIcons.app.toSvg(),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    ShaderMask(
+                      shaderCallback: (bounds) => primaryGradient.createShader(Rect.fromLTWH(0, 0, bounds.width, bounds.height)),
+                      child: const Text(
+                        'DishLocal',
+                        style: TextStyle(
+                          fontFamily: 'SFProDisplay',
+                          fontWeight: FontWeight.w700,
+                          fontSize: 40,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      'Chia sẻ & Khám phá trực tiếp',
+                      style: Theme.of(context).textTheme.titleLarge,
+                      textAlign: TextAlign.center,
+                    ),
+                    Text(
+                      'ẩm thực quanh bạn',
+                      style: Theme.of(context).textTheme.titleLarge,
+                      textAlign: TextAlign.center,
+                    ),
+                  ],
+                ),
+                const Expanded(child: SizedBox()),
+                FilledButton.icon(
+                  onPressed: () {
+                    context.read<AuthBloc>().add(GoogleSignInRequested());
+                  },
+                  label: Text(
+                    'Đăng nhập bằng tài khoản Google',
+                    style: appTextTheme(context).labelLarge!.copyWith(
+                          color: Colors.black,
+                        ),
+                  ),
+                  icon: AppIcons.google.toSvg(
+                    width: 20,
+                  ),
+                  style: FilledButton.styleFrom(
+                    backgroundColor: Colors.white,
+                    padding: const EdgeInsetsDirectional.only(
+                      top: 10,
+                      bottom: 10,
+                      start: 12,
+                      end: 12,
                     ),
                   ),
-                  // Màu nền để dễ thấy
-                  child: AppIcons.app.toSvg(),
                 ),
                 const SizedBox(
-                  height: 10,
+                  height: 15,
                 ),
-                ShaderMask(
-                  shaderCallback: (bounds) => primaryGradient.createShader(Rect.fromLTWH(0, 0, bounds.width, bounds.height)),
-                  child: const Text(
-                    'DishLocal',
-                    style: TextStyle(
-                      fontFamily: 'SFProDisplay',
-                      fontWeight: FontWeight.w700,
-                      fontSize: 40,
-                    ),
-                  ),
+                Text(
+                  'Đăng nhập để tiếp tục...',
+                  style: Theme.of(context).textTheme.titleMedium,
                 ),
                 const SizedBox(
-                  height: 10,
-                ),
-                Text(
-                  'Chia sẻ & Khám phá trực tiếp',
-                  style: Theme.of(context).textTheme.titleLarge,
-                  textAlign: TextAlign.center,
-                ),
-                Text(
-                  'ẩm thực quanh bạn',
-                  style: Theme.of(context).textTheme.titleLarge,
-                  textAlign: TextAlign.center,
+                  height: 20,
                 ),
               ],
             ),
-            const Expanded(child: SizedBox()),
-            FilledButton.icon(
-              onPressed: () {
-                context.read<AuthBloc>().add(GoogleSignInRequested());
-              },
-              label: Text(
-                'Đăng nhập bằng tài khoản Google',
-                style: appTextTheme(context).labelLarge!.copyWith(
-                      color: Colors.black,
-                    ),
-              ),
-              icon: AppIcons.google.toSvg(
-                width: 20,
-              ),
-              style: FilledButton.styleFrom(
-                backgroundColor: Colors.white,
-                padding: const EdgeInsetsDirectional.only(
-                  top: 10,
-                  bottom: 10,
-                  start: 12,
-                  end: 12,
-                ),
-              ),
-            ),
-            const SizedBox(
-              height: 15,
-            ),
-            Text(
-              'Đăng nhập để tiếp tục...',
-              style: Theme.of(context).textTheme.titleMedium,
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-          ],
-        ),
+          );
+        },
       ),
     );
   }
