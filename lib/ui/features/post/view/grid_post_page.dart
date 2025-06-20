e850a29d69dbb49c43198a49dc1e04fcbed96451
danhdelrey <duonghoaidanh21092003@gmail.com>
2025-06-20 15:34:17 +0700

    Update grid_post_page.dart

--- lib/ui/features/post/view/grid_post_page.dart
+++ lib/ui/features/post/view/grid_post_page.dart
@@ -1,11 +1,14 @@
+import 'package:dishlocal/app/theme/theme.dart';
 import 'package:dishlocal/core/dependencies_injection/service_locator.dart';
 import 'package:dishlocal/data/categories/post/model/post.dart';
 import 'package:dishlocal/ui/features/post/bloc/post_bloc.dart';
 import 'package:dishlocal/ui/features/post/view/small_post.dart';
 import 'package:dishlocal/ui/widgets/animated_widgets/fade_slide_up.dart';
+import 'package:dishlocal/ui/widgets/element_widgets/custom_loading_indicator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
+import 'package:shimmer/shimmer.dart';
 
 class GridPostPage extends StatefulWidget {
   const GridPostPage({super.key});
@@ -43,14 +46,28 @@ class _GridPostPageState extends State<GridPostPage> {
                       delay: Duration(milliseconds: index * 100),
                       child: SmallPost(post: post),
                     ),
-                    firstPageProgressIndicatorBuilder: (_) => const Center(
-                      child: CircularProgressIndicator(),
+                    firstPageProgressIndicatorBuilder: (_) => Shimmer.fromColors(
+                      baseColor: appColorScheme(context).surfaceContainerLow,
+                      highlightColor: appColorScheme(context).outline,
+                      child: Column(
+                        children: List.generate(
+                          5,
+                          (_) => Expanded(
+                            child: Row(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                _buildShimmeringSmallPost(),
+                                _buildShimmeringSmallPost(),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
                     ),
                     noItemsFoundIndicatorBuilder: (_) => const Center(
                       child: Text("Không có bài viết nào."),
                     ),
-                    noMoreItemsIndicatorBuilder: (context) => const Text('no more'),
-                    newPageProgressIndicatorBuilder: (context) => const Text('loading'),
+                    newPageProgressIndicatorBuilder: (context) => const Center(child: CustomLoadingIndicator(indicatorSize: 30)),
                   ),
                   gridDelegateBuilder: (int childCount) => const SliverSimpleGridDelegateWithFixedCrossAxisCount(
                     crossAxisCount: 2,
@@ -63,6 +80,75 @@ class _GridPostPageState extends State<GridPostPage> {
           ),
         );
       },
+    );
+  }
+
+  Widget _buildShimmeringSmallPost() {
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+        child: Column(
+          children: [
+            Container(
+              height: 10,
+              decoration: BoxDecoration(
+                color: Colors.amber,
+                borderRadius: BorderRadius.circular(1000),
+              ),
+            ),
+            const SizedBox(
+              height: 5,
+            ),
+            AspectRatio(
+              aspectRatio: 1,
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.amber,
+                  borderRadius: BorderRadius.circular(12),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 5,
+            ),
+            Row(
+              children: [
+                Container(
+                  width: 16,
+                  height: 16,
+                  decoration: BoxDecoration(
+                    color: Colors.amber,
+                    borderRadius: BorderRadius.circular(1000),
+                  ),
+                ),
+                const SizedBox(
+                  width: 5,
+                ),
+                Expanded(
+                  child: Container(
+                    height: 10,
+                    decoration: BoxDecoration(
+                      color: Colors.amber,
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  width: 20,
+                ),
+                Container(
+                  width: 30,
+                  height: 10,
+                  decoration: BoxDecoration(
+                    color: Colors.amber,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
