7557e9b7fa21c6a8e558625b6984b8f265d26b78
danhdelrey <duonghoaidanh21092003@gmail.com>
2025-06-22 09:52:58 +0700

    Switch to production Firebase config and add release signing
    Updated android/app/build.gradle to load signing configs from key.properties and use a release signing config. Replaced development Firebase configuration with production settings in google-services.json, firebase.json, and lib/firebase_options_prod.dart. Removed environment-specific google-services.json files for dev and prod.

--- lib/firebase_options_prod.dart
+++ lib/firebase_options_prod.dart
@@ -51,7 +51,7 @@ class DefaultFirebaseOptions {
 
   static const FirebaseOptions android = FirebaseOptions(
     apiKey: 'AIzaSyAqYD4F7cb4TIGf1Acn98O6S87w4HxZBmw',
-    appId: '1:413476207561:android:49397c1e20a3c763a3c618',
+    appId: '1:413476207561:android:1aa83cc31c965743a3c618',
     messagingSenderId: '413476207561',
     projectId: 'dishlocal-prod',
     storageBucket: 'dishlocal-prod.firebasestorage.app',
@@ -63,6 +63,9 @@ class DefaultFirebaseOptions {
     messagingSenderId: '413476207561',
     projectId: 'dishlocal-prod',
     storageBucket: 'dishlocal-prod.firebasestorage.app',
+    androidClientId: '413476207561-q7affla1klt4fj8co9374uo0dmh9nmu7.apps.googleusercontent.com',
+    iosClientId: '413476207561-rien86thp193hsh3mc89ku6sb3euavfc.apps.googleusercontent.com',
     iosBundleId: 'com.example.dishlocal',
   );
+
 }
