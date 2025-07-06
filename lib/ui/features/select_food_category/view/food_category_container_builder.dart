0c33595cb7b8228a16a9be7514416b31a3cb7400
danhdelrey <duonghoaidanh21092003@gmail.com>
2025-07-06 10:51:06 +0700

    builder

--- lib/ui/features/select_food_category/view/food_category_container_builder.dart
+++ lib/ui/features/select_food_category/view/food_category_container_builder.dart
@@ -5,41 +5,52 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class FoodCategoryContainerBuilder extends StatelessWidget {
-  const FoodCategoryContainerBuilder({super.key, required this.builder});
+  const FoodCategoryContainerBuilder({super.key, required this.builder,  this.allowMultiSelect = false});
 
-  final Widget Function(List<FoodCategory> allCategories, Set<FoodCategory> selectedCategories, bool allowMultiSelect) builder;
+  final Widget Function(BuildContext context,List<FoodCategory> allCategories, Set<FoodCategory> selectedCategories, bool allowMultiSelect) builder;
+  final bool allowMultiSelect;
+
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<SelectFoodCategoryBloc, SelectFoodCategoryState>(
-      builder: (context, state) {
-        // Kiểm tra nếu trạng thái là _Loaded
-        if (state is SelectFoodCategoryLoaded) {
-          // Do state đã được xác nhận là _Loaded, chúng ta có thể
-          // truy cập các thuộc tính của nó một cách an toàn.
-          return Container(
-            height: double.infinity,
-            width: double.infinity,
-            decoration: BoxDecoration(
-              gradient: state.selectedCategories.isNotEmpty
-                  ? LinearGradient(
-                      colors: [
-                        state.selectedCategories.first.color.withValues(alpha: 0.2),
-                        Colors.transparent,
-                      ],
-                      begin: Alignment.topLeft,
-                      end: Alignment.bottomRight,
-                    )
-                  : null,
-            ),
-            child: builder(state.allCategories, state.selectedCategories, state.allowMultiSelect),
-          );
-        } else {
-          // Nếu không phải _Loaded, thì nó là _Initial (hoặc trạng thái khác)
-          // Hiển thị vòng xoay chờ.
-          return const SizedBox();
-        }
-      },
+    return BlocProvider(
+      create: (context) => getIt<SelectFoodCategoryBloc>()
+        ..add(
+           SelectFoodCategoryEvent.initialized(
+            allCategories: FoodCategory.values,
+            allowMultiSelect: allowMultiSelect,
+          ),
+        ),
+      child: BlocBuilder<SelectFoodCategoryBloc, SelectFoodCategoryState>(
+        builder: (context, state) {
+          // Kiểm tra nếu trạng thái là _Loaded
+          if (state is SelectFoodCategoryLoaded) {
+            // Do state đã được xác nhận là _Loaded, chúng ta có thể
+            // truy cập các thuộc tính của nó một cách an toàn.
+            return Container(
+              height: double.infinity,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                gradient: state.selectedCategories.isNotEmpty
+                    ? LinearGradient(
+                        colors: [
+                          state.selectedCategories.first.color.withValues(alpha: 0.2),
+                          Colors.transparent,
+                        ],
+                        begin: Alignment.topLeft,
+                        end: Alignment.bottomRight,
+                      )
+                    : null,
+              ),
+              child: builder(context,state.allCategories, state.selectedCategories, state.allowMultiSelect),
+            );
+          } else {
+            // Nếu không phải _Loaded, thì nó là _Initial (hoặc trạng thái khác)
+            // Hiển thị vòng xoay chờ.
+            return const SizedBox();
+          }
+        },
+      ),
     );
   }
 }
