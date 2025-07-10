b7ab0a3952b92338fd9b884b7fc51a5c1e19acbe
danhdelrey <duonghoaidanh21092003@gmail.com>
2025-07-10 11:20:23 +0700

    Add vertical padding to SliverAppBar in grid post page
    Wrapped the SliverAppBar with a SliverPadding widget to add vertical padding, improving the visual spacing above and below the filter button in the grid post page.

--- lib/ui/features/post/view/grid_post_page.dart
+++ lib/ui/features/post/view/grid_post_page.dart
@@ -97,16 +97,19 @@ class _GridPostPageState extends State<GridPostPage> {
             controller: _scrollController,
             physics: const BouncingScrollPhysics(parent: AlwaysScrollableScrollPhysics()),
             slivers: [
-              SliverAppBar(
-                primary: false,
-                floating: true,
-                snap: true,
-                pinned: false,
-                backgroundColor: Theme.of(context).scaffoldBackgroundColor,
-                surfaceTintColor: Colors.transparent,
-                elevation: 0,
-                automaticallyImplyLeading: false,
-                title: const FilterButton(),
+              SliverPadding(
+                padding: const EdgeInsets.symmetric(vertical: 15),
+                sliver: SliverAppBar(
+                  primary: false,
+                  floating: true,
+                  snap: true,
+                  pinned: false,
+                  backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+                  surfaceTintColor: Colors.transparent,
+                  elevation: 0,
+                  automaticallyImplyLeading: false,
+                  title: const FilterButton(),
+                ),
               ),
               if (state.posts.isEmpty)
                 SliverFillRemaining(
